3c90d9f0c91b2b658a7a79e0c58a71983553e660
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-08-28 21:28:01 +0700

    add layout

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,27 @@
+import Layout from '@/layouts';
 import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 const Home = lazy(() => import('@/features/home/index'));
+const Login = lazy(() => import('@/features/auth/pages/login'));
+const Register = lazy(() => import('@/features/auth/pages/register'));
 
 const router = createBrowserRouter([
   {
-    element: <Home />,
-    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        element: <Home />,
+        path: '/',
+      },
+    ],
+  },
+  {
+    element: <Login />,
+    path: '/login',
+  },
+  {
+    element: <Register />,
+    path: '/register',
   },
 ]);
 export default router;
