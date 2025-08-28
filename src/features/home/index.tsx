e11daf90bdaeb8770e1af8a6249eda1781c5c903
Khanh Vinh <nguyenhongkhanhvinh2511@gmail.com>
2025-08-28 21:28:12 +0700

    add home page

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,4 +1,21 @@
+import PostItem from '../posts/components/post-item';
+import StoryList from './components/story-list';
+
 const Home = () => {
-  return <div className="">Home</div>;
+  return (
+    <div className='w-full flex justify-center px-6 lg:px-10 '>
+      <div className='w-full max-w-[630px]'>
+        <div className='mt-4'>
+          <div className='mb-6'>
+            <StoryList />
+          </div>
+          <div className='flex justify-center'>
+            <PostItem />
+          </div>
+        </div>
+      </div>
+      <div className='w-[320px] h-[100vh] pl-[64px]'></div>
+    </div>
+  );
 };
 export default Home;
